d729f736d67ae9643942ddac3f08ad483467c5f2
juliocesardossantosferreira <santos.ferreira.julio@escola.pr.gov.br>
2025-06-05 11:04:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando que aconteceu a 2° guerra mundial?',
+    'a 2° guerra mundial ocorreu em 1945'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o valor de pi?',
+    ' O valor de pi é aproximadamente 3,14'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
